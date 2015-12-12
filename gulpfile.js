1fce84f22543b9782d9df80bd6c2bfa7fe91e6ee
Alastair Smith <github@alastairsmith.me.uk>
2015-12-12 17:17:18 +0000

    Install and wire up gulp-sass for CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,41 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
     minify = require('gulp-minify');
 
 var templates = './src/templates/**/*.jade';
+var styles = './src/sass/**/*.scss';
 
 gulp.task('templates', function() {
-  return gulp.src(templates)
-             .pipe(jade())
-             .pipe(minify())
-             .pipe(gulp.dest('./build/'))
+  gulp.src(templates)
+    .pipe(jade())
+    .pipe(minify())
+    .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('styles', function() {
+  gulp.src(styles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(minify())
+    .pipe(gulp.dest('./build/css/'))
+});
+
+gulp.task('watch', function() {
+  var sources = [templates, styles];
+  gulp.src(sources)
+    .pipe(watch(sources));
 });
 
 gulp.task('dev', function() {
-  return gulp.src(templates)
-             .pipe(watch(templates))
-             .pipe(jade())
-             .pipe(minify())
-             .pipe(gulp.dest('./build/'))
+  gulp.src(templates)
+    .pipe(watch(templates, {verbose: true}))
+    .pipe(jade())
+    .pipe(gulp.dest('./build/'));
+  gulp.src(styles)
+    .pipe(watch(styles, {verbose: true}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css/'))
 });
 
-gulp.task('default', ['templates']);
+gulp.task('default', ['templates', 'styles']);
